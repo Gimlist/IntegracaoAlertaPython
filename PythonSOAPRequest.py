848ede1e33247d65788cc4f426ec84a98d2874cc
Gimlist <guilhermerod.a@gmail.com>
2019-05-03 16:28:03 -0300

    exclusao arquivo sa

--- PythonSOAPRequest.py
+++ PythonSOAPRequest.py
@@ -1,21 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[83]:
-
-
 import urllib3
 import requests
 
-
-# In[84]:
-
-
 url = 'http://soap.alertafiscalintranet.com.br/WsSolicitacaoCadastro.asmx'
-
-
-# In[85]:
-
 
 headers = {'content-type': 'text/xml'}
 body = """<?xml version="1.0" encoding="utf-8"?>
@@ -32,51 +21,16 @@ xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
  </soap:Body>
 </soap:Envelope>"""
 
-
-# In[86]:
-
-
 response = requests.post(url,data=body,headers=headers)
-
-
-# In[87]:
-
 
 response
 
-
-# In[88]:
-
-
 response. content
-
-
-# In[89]:
-
 
 http = urllib3.PoolManager()
 
-
-# In[90]:
-
-
 r = http.request('GET',url)  
 r.status
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
 
 
 
